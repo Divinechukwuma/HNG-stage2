4597c9db739491b23870f575d831a33ab10d765e
divinechukwuma <divinechukwuma982@gmail.com>
2024-07-02 12:10:24 +0100

    i am awesome

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 header('Content-Type: application/json');
 
+$visitor_name = isset($_GET['visitor_name']) ? $_GET['visitor_name'] : 'Guest';
+
+header('Content-Type: application/json');
+
 // Function to get the client IP address
 function get_client_ip() {
     $ipaddress = '';
@@ -24,21 +28,20 @@ function get_client_ip() {
 
 $visitor_name = isset($_GET['visitor_name']) ? $_GET['visitor_name'] : 'Guest';
 
+// Get client IP
+$client_ip = get_client_ip();
 
-// Get location and temperature (mock data for now)
-
-// Using a free API service to get location and weather data
+// Get location data
 $location_api_url = "https://api.ip2location.io/?key=45C8DDB02866299AFFBBE509C3CC87DA&ip={$client_ip}";
 $location_data = json_decode(file_get_contents($location_api_url), true);
 $city = $location_data['city'];
 
-$temperature = 35; // Replace with actual data from an API
-
 $response = [
-    "client_ip" => get_client_ip(),
-    "location" => $location,
-    "greeting" => "Hello, $visitor_name!, the temperature is $temperature degrees Celsius in $location"
+    "client_ip" => $client_ip,
+    "location" => $city,
+    "greeting" => "Hello, {$visitor_name}!, the temperature is {$temperature} degrees Celsius in {$city}"
 ];
 
 echo json_encode($response);
+?>
 
